b7c9c5b7e986510af617dd153bb02a1cfeceab26
TimBaron <akiodetimothy2017@gmail.com>
2020-10-13 19:04:05 +0100

    Email Verification

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                {{-- <div class="card-header">{{ __('') }}</div> --}}
+                <h5 class="card-header text-center">Verify Your Email Address</h5>
 
                 <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    <h5>Before proceeding, please check your email for a verification link.</h5>
+                    <h5>If you did not receive the email</h5><a href="#" onclick="event.preventDefault();
+                        document.getElementById('link-resend').submit();
+                    ">click here to request another</a>
+                    
+                    <form class="d-inline" id="link-resend" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        {{-- <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>. --}}
                     </form>
                 </div>
             </div>
--- resources/views/messages/flash.blade.php
+++ resources/views/messages/flash.blade.php
@@ -26,3 +26,8 @@
         {{ session('status') }}
     </div>
 @endif
+@if (session('resent'))
+    <div class="alert alert-success" role="alert">
+        {{ __('A fresh verification link has been sent to your email address.') }}
+    </div>
+@endif
