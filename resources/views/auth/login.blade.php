9ae3990df92a017f9bc31ed624039971d5736819
TimBaron <akiodetimothy2017@gmail.com>
2020-10-13 10:12:53 +0100

    Login Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card bg-dark">
@@ -50,7 +50,7 @@
                             </div>
                         </div> --}}
 
-                        <div class="form-group row mb-0">
+                        {{-- <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
@@ -71,6 +71,47 @@
                     </form>
                 </div>
             </div>
+        </div>
+    </div>
+</div> --}}
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col col-md-8">
+            <div class="card mb-3" style="max-width: 700px;">
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                    <img src="{{asset('img/banner/vote5.jpg')}}" class="card-img" alt="..." style="height: 100%">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Login Form</h5>
+                      <form action="{{route('login')}}" class="form-contact contact_form" method="post">
+                        @csrf
+                        <div class="row">
+                            <div class="form-group col-12">
+                                <label for="email">Email Address</label>
+                                <input class="form-control" name="email" id="email" type="email" placeholder="Enter your email" value="{{old('email')}}">
+                            </div>
+                        </div>
+                        <div class="row">
+                          <div class="form-group col-12">
+                              <label for="password">Password</label>
+                              <input class="form-control" name="password" id="password" type="password" placeholder="Enter your Password">
+                          </div>
+                        </div>
+                        <div class="form-group text-center text-md-right mt-3">
+                          <button type="submit" class="button button--active button-contactForm">Login</button>
+                        </div>
+                      </form>
+                      <div class="row justify-content-between">
+                          <p class="card-text"><a href="{{route('register')}}" class="btn btn-default">Sign Up</a></p>
+                          <p class="card-text"><a href="{{route('password.request')}}" class="btn btn-default">Forgot Password?</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
         </div>
     </div>
 </div>
